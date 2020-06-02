79725197e99ebda73c44f3345bbd048f8e9a7621
GarrettJMU <garretthughes3@gmail.com>
2020-06-02 16:32:57 -0700

    add organization schema and change reactga init to useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import logo from './logo.svg'
+import './App.css'
 import ReactGA from 'react-ga'
+import Helmet from 'react-helmet'
 
 const trackingId = "UA-XXXX"
 
 function App() {
-  ReactGA.initialize(trackingId)
+
+  useEffect(() => {
+    ReactGA.initialize(trackingId)
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Helmet>
+        <script type="application/ld+json">
+          {{
+            "@context": "https://schema.org",
+            "@type": "Airline",
+            "name": "HCP",
+            "url": "houscall.com",
+            "logo": "logo.com"
+          }}
+        </script>
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
