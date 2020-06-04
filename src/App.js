777a8c010866796e5eaa14e09c1d035cc3f8f170
GarrettJMU <garretthughes3@gmail.com>
2020-06-04 16:20:15 -0700

    add redirect for /

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom"
 import AppPage from "./AppPage.js"
 import Dummy from "./dummy"
@@ -14,6 +15,9 @@ export default function App() {
         {["/LEARN/BUSINESSES/PLUMBING/NEW/HOW-TO-START-A-PLUMBING-BUSINESS/", "/learn/businesses/plumbing/new/how-to-start-a-plumbing-business/"].map((path, index) =>
           <Route exact path={path} sensitive={true} component={AppPage} key={index} />
         )}
+        <Route exact path="/">
+          <Redirect to="/LEARN/BUSINESSES/PLUMBING/NEW/HOW-TO-START-A-PLUMBING-BUSINESS/" />
+        </Route>
         <Route component={Dummy}/>
       </Switch>
     </Router>
